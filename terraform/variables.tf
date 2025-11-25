d57e15709bc0dc4b03d74750402626b9f28ce319
waynerferreira <waynerferreira@hotmail.com>
2025-11-25 16:42:04 -0300

    ci:add iam rules and adjustments on variables data backend

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,7 @@ variable "region" {}
 variable "service_name" {}
 variable "cluster_name" {}
 variable "service_memory" {}
+variable "service_healthcheck" {}
 variable "service_cpu" {}
 variable "service_port" {}
 
@@ -12,3 +13,9 @@ variable "ssm_private_subnet_2" {}
 variable "ssm_private_subnet_3" {}
 variable "ssm_listener" {}
 
+variable "environment_variables" {}
+variable "capabilities" {}
+
+
+variable "service_launch_type" {}
+variable "service_task_count" {}
